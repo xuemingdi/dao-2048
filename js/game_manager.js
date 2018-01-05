905da162d63eccbe4098bb7fcae97a3c4f193546
xuemingdi <xuemingdi@hotmail.com>
2018-01-05 16:03:23 +0800

    for DCE CI/CD testing

--- js/game_manager.js
+++ js/game_manager.js
@@ -4,7 +4,7 @@ function GameManager(size, InputManager, Actuator, StorageManager) {
   this.storageManager = new StorageManager;
   this.actuator       = new Actuator;
 
-  this.startTiles     = 2;
+  this.startTiles     = 1;
 
   this.inputManager.on("move", this.move.bind(this));
   this.inputManager.on("restart", this.restart.bind(this));
@@ -45,7 +45,7 @@ GameManager.prototype.setup = function () {
     this.keepPlaying = previousState.keepPlaying;
   } else {
     this.grid        = new Grid(this.size);
-    this.score       = 100;
+    this.score       = 10;
     this.over        = false;
     this.won         = false;
     this.keepPlaying = false;
